9598c06d2ccaaf15e512c039cb1939f6adfb2c1e
LmaAl-Johare1 <lmaref.j@gmail.com>
2024-08-02 16:24:21 +0300

    my solution to remove the node from the end of the sigly linked list

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace LinkedList
+{
+   
+    class Program
+    {
+        static void PrintList(ListNode head)
+        {
+            while (head != null)
+            {
+                Console.Write(head.val + " -> ");
+                head = head.next;
+            }
+            Console.WriteLine("NULL");
+        }
+
+        static ListNode CreateLinkedList()
+        {
+            ListNode head = null;
+            ListNode tail = null;
+
+            Console.WriteLine("Enter the values for the linked list (enter -1 to end input): ");
+            while (true)
+            {
+                int value = int.Parse(Console.ReadLine());
+                if (value == -1) break;
+
+                ListNode newNode = new ListNode(value);
+                if (head == null)
+                {
+                    head = newNode;
+                    tail = head;
+                }
+                else
+                {
+                    tail.next = newNode;
+                    tail = tail.next;
+                }
+            }
+            return head;
+        }
+
+        static void Main()
+        {
+            ListNode head = CreateLinkedList();
+
+            Console.WriteLine("Enter the position n to remove from the end: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Original List: ");
+            PrintList(head);
+
+            RemoveNode solution = new RemoveNode();
+            head = solution.RemoveNthFromEnd(head, n);
+
+            Console.WriteLine("Modified List after removing {0}th node from the end: ", n);
+            PrintList(head);
+        }
+    }
+}
---
+++ RemoveNode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    internal class RemoveNode
+    {
+      
+            internal ListNode RemoveNthFromEnd(ListNode head, int n)
+            {
+                if (head == null)
+                {
+                    return null;
+                }
+
+                int length = 0;
+                ListNode curr = head;
+                while (curr != null)
+                {
+                    length++;
+                    curr = curr.next;
+                }
+
+                int count = 0;
+                curr = head;
+                ListNode prev = null;
+                while (count < length - n)
+                {
+                    prev = curr;
+                    curr = curr.next;
+                    count++;
+                }
+
+                if (prev == null)
+                {
+                    head = head.next;
+                }
+                else
+                {
+                    prev.next = curr.next;
+                }
+
+                return head;
+           }
+        
+
+    }
+}
